73496d5cc07a3ebda79dfa1148125ce5469c9da8
Brianwu24 <brian.wu24@ycdsbk12.ca>
2024-04-05 23:11:52 -0400

    Update, bug fix

--- Assets/PowerUps/BasePowerUp.cs
+++ Assets/PowerUps/BasePowerUp.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BasePowerUp
 {
+    private GameController _gameController;
+    private string _name;
+    private string _color;
     private float _scoreIncrease;
-    private GameController _game;
 
-    public BasePowerUp(float score)
+    public BasePowerUp(GameController gamecontroller, string color, string name, float score)
     {
+        _gameController = gamecontroller;
+        _name = name;
+        _color = color;
         _scoreIncrease = score;
     }
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
 
